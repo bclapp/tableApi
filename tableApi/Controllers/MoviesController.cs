2ae811171064fa18d5de525d9a4cfc2afb255900
bclapp <bclapp2012@eagles.ewu.edu>
2016-06-30 21:38:35 -0700

    first commit

---
+++ tableApi/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using tableApi.Models;
+
+namespace tableApi.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        
+        Movie[] mvs = new Movie[] { new Movie { movieId = 500, price = 1.00, studio = "testStudio", title = "testTitle", year = 1994} };
+
+        //DB connection
+        MovieDb dbConnection = new MovieDb { dbname = "bradentc_movie_managment", password = "something", servername = "localhost", username = "bradentc_movie" };
+        
+        //TEST
+
+        //PUT
+
+        //POST
+        
+        //GET
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = mvs.FirstOrDefault((p) => p.movieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+
+        //DELETE
+
+        //OPTIONS
+
+        public IEnumerable<Movie> ProcessMovies()
+        {
+            return mvs;
+        }
+
+   
+    }
+
+    
+}
---
+++ tableApi/Models/Movies.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tableApi.Models
+{
+    public class Movie
+    {
+        public int movieId { get; set; }
+        public string title { get; set; }
+        public double price { get; set; }
+        public string studio { get; set; }
+        public int year { get; set; }
+
+    }
+
+    public class MovieDb
+    {
+        public string servername { get; set; }
+        public string username  { get; set; }
+        public string password { get; set; }
+        public string dbname { get; set; }
+    }
+
+}
